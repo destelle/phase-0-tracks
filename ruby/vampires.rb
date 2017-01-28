40ab3bca0d59f5468ba7644250676f563d70ddca
Dillon M. Estelle <destelle.de@gmail.com>
2017-01-27 16:31:58 -0800

    first test for name and age

---
+++ ruby/vampires.rb
@@ -0,0 +1,6 @@
+puts "What is your name?";
+name = gets;
+puts "Hello" + name + ". How old are you? what year were you born?";
+age = gets;
+year = gets;
+puts = "age is " + age + ", year born is " + year;
