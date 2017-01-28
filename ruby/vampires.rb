f190c0caf88460cc99594c70c083983c843d558b
Dillon M. Estelle <destelle.de@gmail.com>
2017-01-27 17:08:30 -0800

    bread and health insuranse booleans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,3 +11,23 @@ myAge = gets.chomp.to_i
 puts "What year were you born"
 myYear = gets.chomp.to_i
 puts "#{myName} is #{myAge}, born in #{myYear}!"
+
+puts "do you like garlic bread? (yes or no)"
+bread = gets.chomp
+case bread
+	when 'y','Y','yes'
+		bread = true;
+	when 'n','N','no'
+		bread = false
+end
+puts bread
+
+puts "do you want to enroll in health insurance? (yes or no)"
+health = gets.chomp
+case health
+	when 'y','Y','yes'
+		health = true;
+	when 'n','N','no'
+		health = false
+end
+puts health
