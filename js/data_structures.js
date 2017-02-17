2244727959f6f9b4b0d42e053e195b227aa075f8
Dillon M. Estelle <destelle.de@gmail.com>
2017-02-17 12:56:51 -0800

    phase 2 pairing commit

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,28 @@ for(var i =  0; i < colors.length; i++){
 	horse[names[i]] = colors[i];
 }
 console.log(horse);
+
+function Car(c,b,y){
+	this.color = c;
+	this.brand = b;
+	this.year = y;
+
+	this.speed = function() {console.log("Current speed is 60");};
+};
+
+var car = new Car("gray","honda", 2000);
+var car2 = new Car("blue", "toyota", 2005);
+var car3 = new Car("red", "ford", 1999);
+
+console.log(car);
+car.speed();
+
+console.log("-----");
+
+console.log(car2);
+car2.speed();
+
+console.log("-----");
+
+console.log(car3);
+car3.speed();
