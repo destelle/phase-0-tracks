4eb39b48b5eacf4b138e30aed0027f37cac156c3
Dillon M. Estelle <destelle.de@gmail.com>
2017-02-17 12:26:16 -0800

    phase 0 pairing commit

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "red", "yellow", "orange"];
+var names = ["ED", "Daisy", "Bolt", "Sam"];
+
+colors.push("purple");
+names.push("Hank");
+
+console.log(colors);
+console.log(names);
